fa6938c5121f9bbbad4400ec989f145e809e04b1
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-01-28 18:12:54 -0500

    the letters are displaying but the shap is not displaying on the browser, still needs to get the dimensions of the shape to be prompted

--- utils/generateSVG.js
+++ utils/generateSVG.js
@@ -1,4 +1,15 @@
-function generateSVG() {} 
+// In generateSVG.js
+function generateSVG(userInput) {
+    //deconstruct userInput
+    const { text, textColor, shape, shapeColor } = userInput;
 
+    //Customizing SVG template based on the SVG prompted questions
+    const svgTemplate = `
+        <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+            <text x="10" y="40" fill="${textColor}">${text}</text>
+        </svg>
+    `;
+    return svgTemplate;
+} 
 
 module.exports = generateSVG
