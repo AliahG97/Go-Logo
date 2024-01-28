587e4370a3920d9a35fb6941766cfae93d51ace9
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-01-28 03:34:41 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateSVG = require('./package-lock.json');
+
+// Create an array of questions for user input
+//const questions = [text, textColor, shape, shapeColor]
+
+const prompts = [
+    {
+        type:'input',
+        name:'text',
+        message:'Enter up to three characters for the text:',
+        validate: (input) => input.length <= 3,
+
+    },
+    {
+        type:'input',
+        name:'textColor',
+        message:'Enter the text color:',
+    },
+    {
+        type:'list',
+        name:'shape',
+        message:'Chose a shape:',
+        choices: ['circle', 'triangle', 'square'],
+    },
+    {
+        type:'input',
+        name:'shapeColor',
+        message:'Enter the shape color:',
+    },
+];
+async function init() {
+    try {
+        const userInput = await inquirer.prompt(prompts);
+        const svgContent = generateSVG(userInput);
+        fs.writeFileSync('logo.svg', svgContent);
+        console.log('Generate logo.svg');
+    } catch (error) {
+        console.error('Error generating svg file:', error);
+    }
+}
+init();
